8c61a0dd9cdfd4188d8fefc0ff6cc1bd3623dd33
Rafael Brito <rab405@g.harvard.edu>
2020-03-31 15:55:16 -0500

    adding covid data

---
+++ setup.sh
@@ -0,0 +1,3 @@
+alias python=python3
+export ML_PATH="/Users/rbrito/ML"
+export PATH=$PATH:/Users/rbrito/Library/Python/3.6/lib/python/site-packages
---
+++ start.sh
@@ -0,0 +1,16 @@
+# all code here
+# https://github.com/ageron/handson-ml/blob/master/02_end_to_end_machine_learning_project.ipynb
+
+source setup.sh
+
+#to start, just type
+virtualenv env
+
+#then
+
+# just when required....
+#pip3 install --upgrade jupyter matplotlib numpy pandas scipy scikit-learn tensorflow pillow keras h5py opencv-python gensim nltk spacy librosa ffmpeg seaborn plotly xlrd
+
+#then
+
+jupyter notebook
